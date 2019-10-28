9375bb0b2327cc1e2a78377d4c71c3f5de6dddf6
Yusuf ŞENEL <ysf.senel@gmail.com>
2019-10-28 23:26:13 +0300

    register and login view

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,15 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 //  res.render('index', { title: 'Express' });
-  res.render('register', { title: 'Express' });
+  res.render('index', { title: 'Express' });
+});
+router.get('/login', function(req, res, next) {
+//  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'Login' });
+});
+router.get('/register', function(req, res, next) {
+//  res.render('index', { title: 'Express' });
+  res.render('register', { title: 'Register' });
 });
 
 module.exports = router;
