c66f792e36f0e8cdd97bcf084d2d7f9d78735bd0
Yusuf ŞENEL <ysf.senel@gmail.com>
2019-10-30 20:09:29 +0300

    User Model

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,6 @@ router.get('/login', function(req, res, next) {
 //  res.render('index', { title: 'Express' });
   res.render('login', { title: 'Login' });
 });
-router.get('/register', function(req, res, next) {
-//  res.render('index', { title: 'Express' });
-  res.render('register', { title: 'Register' });
-});
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,39 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//Models
+
+const User = require('../models/User');
+
+
+router.get('/register', function(req, res, next) {
+
+  res.render('register', { title: 'Register' });
+});
+
+
+
+router.post('/register',(req,res,next)=>{
+
+  let user=new User();
+  user.firstname=req.body.firstname;
+  user.lastname=req.body.lastname;
+  user.email=req.body.email;
+  user.username=req.body.username;
+  user.password=req.body.password;
+  user.password2=req.body.password2;
+
+  user.save((err,data)=>{
+
+    if (err) {
+      console.log(err);
+      res.json(err);
+    }else {
+        res.json(data);
+    }
+
+  });
+
 });
 
 module.exports = router;
